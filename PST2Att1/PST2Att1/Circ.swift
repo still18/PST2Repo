2c3e843821fde057b7eb54029b2cbf046209bf09
Sean Tillman <sean.tillman18@gmail.com>
2020-09-24 23:10:06 -0400

    Added a button to test inputs

---
+++ PST2Att1/PST2Att1/Circ.swift
@@ -0,0 +1,24 @@
+//
+//  Circ.swift
+//  PST2Att1
+//
+//  Created by Sean on 9/24/20.
+//  Copyright © 2020 SeanTillman. All rights reserved.
+//
+
+import SwiftUI
+
+struct Circ: View {
+    var body: some View {
+        Image("stock1")
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/).overlay(
+            Circle().stroke(Color.gray, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct Circ_Previews: PreviewProvider {
+    static var previews: some View {
+        Circ()
+    }
+}
