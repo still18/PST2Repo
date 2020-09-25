5734e7f6f6fb9cac1780714eeff3fec4f07624cd
Sean Tillman <sean.tillman18@gmail.com>
2020-09-24 20:42:03 -0400

    Trying to add Swift Project

---
+++ PST2Att1/PST2Att1Tests/PST2Att1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  PST2Att1Tests.swift
+//  PST2Att1Tests
+//
+//  Created by Sean on 9/24/20.
+//  Copyright © 2020 SeanTillman. All rights reserved.
+//
+
+import XCTest
+@testable import PST2Att1
+
+class PST2Att1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
