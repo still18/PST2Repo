35e122bd2812b86f8087c60db3030630195235d1
eliruckle <71677202+eliruckle@users.noreply.github.com>
2020-10-18 03:04:46 -0400

    record module and some playback
    reocrd module with start of basic playback

---
+++ record button/record buttonTests/record_buttonTests.swift
@@ -0,0 +1,34 @@
+//
+//  record_buttonTests.swift
+//  record buttonTests
+//
+//  Created by Eli Ruckle on 10/18/20.
+//  Copyright © 2020 Eli Ruckle. All rights reserved.
+//
+
+import XCTest
+@testable import record_button
+
+class record_buttonTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
